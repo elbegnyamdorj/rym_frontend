d14669a1864083995374d314c7a20564c532c602
= <=>
2021-12-14 22:39:04 +0800

    from lap 12/14

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import BaseHome from './components/teacher-home';
 import CardList from './components/lesson-card-list';
 import CreateLesson from './components/add-lesson';
 import CreateSubgroup from './components/add-subgroup';
+import SubgroupList from './components/subgroup-list';
 // import { Routes, Route, Link, Router } from "react-router-dom";
 import {
   BrowserRouter as Router,
@@ -22,9 +23,10 @@ function App() {
               <Route exact path={"/login"} element={<Login />}/>
               <Route exact path={"/signup"} element={<Signup />}/>
               <Route exact path={"/hello"} element={<Hello />}/>
-              <Route exact path={'/home'} element={<BaseHome children={CardList}/>}/>
-              <Route exact path={'/create-lesson'} element={<CreateLesson />}/>
-              <Route exact path={'/create-subgroup'} element={<CreateSubgroup />}/>
+              <Route exact path={'/lesson'} element={<BaseHome children={CardList}/>}/>
+              <Route exact path={'/lesson/create'} element={<CreateLesson />}/>
+              <Route exact path={'/lesson/subgroups/create'} element={<CreateSubgroup />}/>
+              <Route exact path={'/lesson/subgroups'} element={<SubgroupList />}/>
               {/* <Route path={"/"} render={() => <div>Home again</div>}/> */}
           </Routes>
       </main>
---
+++ src/components/subgroup-list/index.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Navbar from "../navbar";
+import Button from "react-bootstrap/Button";
+export class SubgroupList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-1"></div>
+            <div className="col-10 bg-light px-5 ">
+              <h4 className="border-bottom my-5 pb-1">СЭДВҮҮД</h4>
+              <Button className="btn btn-outline-dark btn-lg py-2 mt-4 btn-light btn-block w-100">
+                +
+              </Button>
+            </div>
+            <div className="col-1"></div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default SubgroupList;
