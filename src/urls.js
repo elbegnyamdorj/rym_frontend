8b5d2cc5995689207c758d5691be2c647d414e58
Nyamdorj <b18fa1410@ufe.edu.mn>
2021-12-22 03:37:52 +0800

    to digitalocean

--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,4 @@
-export const MAIN_URL = 'http://localhost:8000/api';
+// export const MAIN_URL = 'http://localhost:8000/api';
 // export const MAIN_URL = 'http://localhost:8000';
+export const MAIN_URL = 'http://159.223.154.42/api';
 
